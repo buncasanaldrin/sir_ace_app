c7e975c87c069ff2746f8b6b708ce69e198497f5
buncasanaldrin <buncasanaldrin@gmail.com>
2023-08-10 16:24:33 +0800

    Fixed - Middleware setup

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -6,16 +6,15 @@ import { fetchUser } from "@/lib/mongoose/actions/user.actions";
 
 const CreateThreadPage: React.FC = async () => {
   const user = await currentUser();
-
   if (!user) return null;
 
+  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
-
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <>
-      <h1 className="head-text">Create Threads</h1>
+      <h1 className="head-text">Create Thread</h1>
 
       <PostThread userId={userInfo._id} />
     </>
